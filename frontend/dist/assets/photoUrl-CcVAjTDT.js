c2ae8e086ab9477e10bc4c22b1f779228ae1770b
amphildanny <philipdaniel.philip@gmail.com>
2026-09-04 02:35:21 +0100

    Force add dist files

---
+++ frontend/dist/assets/photoUrl-CcVAjTDT.js
@@ -0,0 +1,2 @@
+const r="https://ysvqvrskwyyjbeepbyuc.supabase.co",o="profile-photos",a=`${r}/storage/v1/object/public/${o}`;function p(t,n){return t?t.startsWith("http")||t.startsWith("data:")?t:`${a}/${t}`:null}export{p};
+//# sourceMappingURL=photoUrl-CcVAjTDT.js.map
